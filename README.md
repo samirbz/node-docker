49b86912f15f5f43a14607e799e025f0e7ba23e4
root <root@DESKTOP-UPSU5HR>
2024-06-09 10:53:31 +0545

    crud operation done for postman

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ To start docker container
 - docker compose -f docker-compose.yml -f docker-compose.dev.yml up -d
 To stop docker container
 - docker compose -f docker-compose.yml -f docker-compose.dev.yml down
-To check logs
+To check logs - this is used to check the exact terminal like listening port , databasec onnect
 - docker logs node-docker-node-app-1 -f
 To go to mongo shell
 - docker exec -it node-docker-mongo-1 mongosh -u "sameer" -p "mypassword"
--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require("express")
 const mongoose = require("mongoose")
 const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config")
 const app = express()
-const port = process.env.PORT || 4000
 
+const port = process.env.PORT || 4000
+const postRouter = require('./routes/postRoutes')
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
 
 //not best practice to handle all we want to do is dont rely on orchestration docker contaier no one can gurantee that you mongodb is fully running before application starts. make sure your application is intelligent to hadle scenario
@@ -19,9 +20,15 @@ const connectWithRetry = () => {
 
 connectWithRetry()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send("<h2>Hi There!</h2>")
 })
+
+//localhost:4000/api/v1/post/
+app.use('/api/v1/posts', postRouter)
+
 
 app.listen(port, () => {
     console.log(`Listening at port ${port}`)
